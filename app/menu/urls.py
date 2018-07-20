ea83446e7d558f1980e962174a02617c5a802eee
Ryanden <lockstom@gmail.com>
2018-07-20 16:09:41 +0900

    crawling 추가 및 db연동

--- app/menu/urls.py
+++ app/menu/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'menu'
 
 urlpatterns = [
-
     path('', views.menu_list, name='menu-list'),
-
+    path('detail/<int:pk>', views.menu_detail, name='menu-detail')
 ]
--- app/menu/views.py
+++ app/menu/views.py
@@ -4,9 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 
 
-
 def menu_list(request):
 
     return render(request, 'menus/menu_list.html')
 
 
+def menu_detail(request, pk):
+
+    pass
+    # return
